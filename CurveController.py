46ef06a9f21cb405096b4cd6f163fcb925618d6a
mathstracted <mathstracted@gmail.com>
2022-09-22 22:03:02 +0530

    Buggy implementation of cross fade added.

--- CurveController.py
+++ CurveController.py
@@ -9,6 +9,7 @@ bl_info = {
     "category": "Animation",
 }
 
+
 class FrameRangeSelectorForCurveRateSelection(FrameRangeSelector):
     rate: bpy.props.FloatProperty(name="Rate")
 
@@ -25,6 +26,10 @@ class CurveController(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         active_object = context.active_object
+        scene = context.scene
+        
+        row = layout.row()
+        row.operator('animatrickery.toggle_frame_change_listeners')
 
         if(active_object.type != 'CURVE'):
             row = layout.row()
@@ -77,6 +82,25 @@ class RegisterCurveWithController(bpy.types.Operator):
 
         return {'FINISHED'}
 
+class ToggleFrameChangeListeners(bpy.types.Operator):
+    bl_idname = 'animatrickery.toggle_frame_change_listeners'
+    bl_label = "Toggle Frame Change Listeners"
+
+    switch: bpy.props.BoolProperty()
+
+    def execute(self, context):
+        try:
+            if context.scene.animatrickery_frame_change_pre_handler_running:
+                bpy.app.handlers.frame_change_pre.remove(curve_controller_animation_handler)
+        
+            else:
+                bpy.app.handlers.frame_change_pre.append(curve_controller_animation_handler)
+        except:
+            pass
+
+        context.scene.animatrickery_frame_change_pre_handler_running = not context.scene.animatrickery_frame_change_pre_handler_running
+        return {'FINISHED'}
+
 def handle_currve_progression(scene, curve_data):
     
     frame_ranges = curve_data.animatrickery_frame_ranges
@@ -89,47 +113,57 @@ def handle_currve_progression(scene, curve_data):
     active_range = None
     predecessor_range = None
     successor_range = None
-    index_active = -1
     accumulated_eval_time = 0
-
-    for range in frame_ranges:
-        index_active += 1
-
+    frame_range_len = len(frame_ranges)
+    accP = 0
+    accC = 0
+    for (idx, range) in enumerate(frame_ranges):
         if range.end_frame < frame_current:
             accumulated_eval_time += (range.end_frame - range.start_frame) * range.rate
-            pass
+
+            if idx < frame_range_len - 1:
+                next_range = frame_ranges[idx + 1]
+                accumulated_eval_time += (range.end_frame - next_range.start_frame) * (next_range.rate - range.rate) / 2 
+            accP = accumulated_eval_time
         else:
             active_range = range
-            if index_active < len(frame_ranges) - 2:
-                successor_range = frame_ranges[index_active + 1]
-                predecessor_range = frame_ranges[index_active - 1]
+            corrected_start_frame = max(active_range.start_frame, frame_ranges[idx - 1].end_frame)
+            accumulated_eval_time += (frame_current - corrected_start_frame) * active_range.rate
+            
+            if idx < frame_range_len - 1:
+                next_range = frame_ranges[idx + 1]
+
+                if next_range.start_frame <= frame_current:
+                    accumulated_eval_time += (frame_current - next_range.start_frame) * (next_range.rate - range.rate) * (frame_current - next_range.start_frame) / (2 * range.end_frame - next_range.start_frame)
+            accC = accumulated_eval_time
             break
-    
-    if not active_range:
-        return
-    
-    curve_data.eval_time = accumulated_eval_time + (frame_current - active_range.start_frame) * active_range.rate
+    print(accP, accC, accumulated_eval_time)
+    curve_data.eval_time = accumulated_eval_time
 
 
 
 def curve_controller_animation_handler(scene):
-    print("Number of the curves registered", len(scene.animatrickery_curves))
     for curve_details in scene.animatrickery_curves:
         print("Found", bpy.data.curves[curve_details.curve_name].name)
         handle_currve_progression(scene, bpy.data.curves[curve_details.curve_name])
 
 def register():
+    register_classes()
+    register_types()
+
+def unregister():
+    unregister_classes()
+
+def register_classes():
     bpy.utils.register_class(FrameRangeSelectorForCurveRateSelection)
     bpy.utils.register_class(AddRange)
     bpy.utils.register_class(AnimatrickeryCurveDetails)
     bpy.utils.register_class(RegisterCurveWithController)
     bpy.utils.register_class(CurveController)
-    register_types()
-    register_handlers()
+    bpy.utils.register_class(ToggleFrameChangeListeners)
 
-      
-def unregister():
-    unregister_handlers()
+def unregister_classes():
+    bpy.utils.register_class(ToggleFrameChangeListeners)
     bpy.utils.unregister_class(CurveController)
     bpy.utils.unregister_class(RegisterCurveWithController)
     bpy.utils.unregister_class(AnimatrickeryCurveDetails)
@@ -139,9 +173,4 @@ def unregister():
 def register_types():
     bpy.types.Curve.animatrickery_frame_ranges = bpy.props.CollectionProperty(type=FrameRangeSelectorForCurveRateSelection)
     bpy.types.Scene.animatrickery_curves = bpy.props.CollectionProperty(type=AnimatrickeryCurveDetails)
-
-def register_handlers():
-    bpy.app.handlers.frame_change_pre.append(curve_controller_animation_handler)
-
-def unregister_handlers():
-    bpy.app.handlers.frame_change_pre.remove(curve_controller_animation_handler)
+    bpy.types.Scene.animatrickery_frame_change_pre_handler_running = bpy.props.BoolProperty()
